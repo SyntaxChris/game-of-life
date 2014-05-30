81584955d2d8b7ec6e321250aa91d836f0f14d6f
Chris Lehneis <cdlehneis@gmail.com>
2014-05-29 23:21:21 -0400

    Logic complete, need to print to console

--- game_of_life.rb
+++ game_of_life.rb
@@ -22,23 +22,23 @@ class World
                 @cell_grid[y][x].scan << @cell_grid[y][x-1].state
                end
                # Bottom Left
-               if y < 2 && x > 0
+               if y < @rows - 1 && x > 0
                 @cell_grid[y][x].scan << @cell_grid[y+1][x-1].state
                end
                # Bottom
-               if y < 2
+               if y < @rows - 1
                 @cell_grid[y][x].scan << @cell_grid[y+1][x].state
                end
                # Bottom Right
-               if y < 2 && x < 2
+               if y < @rows - 1 && x < @cols - 1
                 @cell_grid[y][x].scan << @cell_grid[y+1][x+1].state
                end
                # Right
-               if x < 2
+               if x < @rows - 1
                 @cell_grid[y][x].scan << @cell_grid[y][x+1].state
                end
                # Top Right
-               if y > 0 && x < 2
+               if y > 0 && x < @cols - 1
                 @cell_grid[y][x].scan << @cell_grid[y-1][x+1].state
                end
                # Top
@@ -55,6 +55,38 @@ class World
         y += 1
         end
     end
+
+    def tick(frames)
+      frames.times do
+        self.scan_neighbors
+        @cell_grid.each do |row|
+          row.each do |cell|
+            case
+            when cell.state == 1
+              if cell.scan.count(1) < 2
+                cell.state = 0
+                cell.scan.clear
+              elsif cell.scan.count(1) > 3
+                cell.state = 0
+                cell.scan.clear
+              else
+                cell.state = 1
+                cell.scan.clear
+              end
+            when cell.state == 0
+              if cell.scan.count(1) == 3
+                cell.state = 1 #regenerate!
+                cell.scan.clear
+              else
+                cell.state = 0
+                cell.scan.clear
+              end
+            end
+          end
+        end
+      end
+    end
+
 end
 
 class Cell
@@ -75,3 +107,7 @@ class Cell
         end
     end
 end
+
+my_world = World.new(3,3)
+
+my_world.tick(1)
